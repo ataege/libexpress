edacd305d2c011d6d885e2aea557793f80ec459d
Ata Ege Asiltürk <ataegeasilturk@gmail.com>
2024-12-10 17:57:52 +0100

    add return value and error handling

--- lib/decorators/routeMapper.ts
+++ lib/decorators/routeMapper.ts
@@ -1,16 +1,50 @@
 import 'reflect-metadata';
-import { HttpRequestMethod, MetadataKeys, RouteDecoratorFactory } from '../types';
+import { HttpRequestMethod, MetadataKeys } from '../types';
+import { defineMetadata } from '../managers/metadataManager';
+import { Request, Response } from 'express';
+import { HttpError } from '../types/errors/HttpError';
+import { isEmpty } from 'lodash'
 
 /**
  * @description Decorator factory function that returns a decorator function
  * @param {HttpRequestMethod} method
  * @returns {MethodDecorator}
  */
-export const routeMapper = (method: HttpRequestMethod): RouteDecoratorFactory => {
-	return (path: string = '/') => {
-		return (target: Object, propertyKey: string | symbol, _desc: TypedPropertyDescriptor<any>) => {
-			Reflect.defineMetadata(MetadataKeys.PATH, path, target, propertyKey)
-			Reflect.defineMetadata(MetadataKeys.METHOD, method, target, propertyKey)
-		};
-	};
-}
+export const routeMapper = (method: HttpRequestMethod) => {
+  return (path: string = '/') => {
+    return function (
+      target: Object,
+      propertyKey: string | symbol,
+      descriptor: TypedPropertyDescriptor<any>
+    ) {
+
+    const originalMethod = descriptor.value as Function;
+
+		// Handle the return value of the function and use it to send response
+		descriptor.value = function(...args: [Request, Response, ...any]) {
+			const res = args[1];
+      try {
+			  const returnValue = originalMethod.apply(this, args);
+      	
+        if (returnValue instanceof Promise) {
+          return returnValue.then((data) => res.send(data)).catch((err) => res.status(500).send(err));
+        } else {
+          return res.send(returnValue);
+        }
+      } catch (err: any) {
+        if (err instanceof HttpError) {
+          return res.status(err.status).send(isEmpty(err.payload) ? err : err.payload);
+        } else {
+          return res.status(500).send({ message: err.message || 'Internal Server Error' });
+        }
+      }
+
+      // TODO Add optional error handler to handle errors
+
+		}
+
+    defineMetadata(MetadataKeys.PATH, path, target, propertyKey);
+    defineMetadata(MetadataKeys.METHOD, method, target, propertyKey);
+    };
+  };
+};
